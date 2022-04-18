43fdfca197dbf60bc73dec2db64ba2a67b6860ca
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2022-04-19 02:58:51 +0300

    more database configuration

---
+++ database/config/build.sql
@@ -0,0 +1,31 @@
+BEGIN;
+    DROP TABLE IF EXISTS users, products, categories;
+    CREATE TABLE users
+    (
+        id SERIAL PRIMARY KEY,
+        role VARCHAR(255) NOT NULL,
+        username VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL
+    );
+    
+    ALTER TABLE
+    users ADD CONSTRAINT users_email_unique UNIQUE(email);
+
+    CREATE TABLE categories
+    (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL
+    );
+
+    CREATE TABLE products
+    (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        description TEXT NOT NULL,
+        price FLOAT NOT NULL,
+        category_id INT NOT NULL,
+        CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(id)
+    );
+
+    COMMIT;
