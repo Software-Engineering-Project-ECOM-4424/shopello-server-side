e5d6782b5ad2d261b2f806b8abb567c535a150d7
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2022-05-02 23:44:49 +0300

    remove res 401

--- utils/jwt/index.js
+++ utils/jwt/index.js
@@ -3,7 +3,6 @@ const { verify, sign } = require('jsonwebtoken');
 const verifyToken = (token, secret) => new Promise((resolve, reject) => {
   verify(token, secret, (err, decode) => {
     if (err) {
-      err.status(401);
       reject(err);
     } else resolve(decode);
   });
@@ -12,7 +11,6 @@ const verifyToken = (token, secret) => new Promise((resolve, reject) => {
 const signToken = (payload, secret) => new Promise((resolve, reject) => {
   sign(payload, secret, (err, token) => {
     if (err) {
-      err.status(401);
       reject(err);
     } else resolve(token);
   });
