e2e976297ef62d714b4b7ae4a86b194076234209
MohammedHasan-99 <mo7ammed.mah@gmail.com>
2022-05-09 08:30:08 +0300

    handle product details request

--- routes/Auth/index.js
+++ routes/Auth/index.js
@@ -133,4 +133,18 @@ router.get('/products/category',
         }
     });
 
+router.get('/product-details',
+    async (req, res) => {
+        try {
+            const { rows } = await dbContext.query('SELECT id,name,description,price, image, category_id FROM products WHERE id = $1', [req.headers.product_id])
+            if (rows.length > 0) {
+                return res.status(200).json(rows);
+            }
+            else return res.status(500).json({ message: "Product Unavailable" });
+        } catch (error) {
+            // throw error
+            return res.status(500).json(error);
+        }
+    });
+
 module.exports = router;
