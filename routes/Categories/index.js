0056346f53d9936b8066822b40f0a2c20ab2cc5b
OsamaRaed <63412382+OsamaRaed@users.noreply.github.com>
2022-05-14 18:08:45 +0300

    add categories

---
+++ routes/Categories/index.js
@@ -0,0 +1,84 @@
+'use strict';
+const router = require('express').Router();
+const dbContext = require('../../database/connection')
+const { body, validationResult } = require('express-validator');
+const checkAdmin = require('../../middlewares/check-admin');
+
+router.get('/',
+    async (req, res) => {
+        try {
+            const { rows } = await dbContext.query('SELECT * FROM categories');
+            return res.status(200).json(rows);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+);
+
+
+router.post('/',
+    // checkAdmin,
+    body('name').isLength({ min: 3 }),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+        try {
+            const { name } = req.body;
+            const result = await dbContext.query('INSERT INTO categories(name) VALUES ($1) RETURNING *;'
+                , [name]);
+            res.status(201).json(result.rows[0]);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
+);
+
+router.get('/:id',
+    async (req, res) => {
+        const id = req.params.id;
+        try {
+
+            const { rows } = await dbContext.query('SELECT * FROM categories WHERE id = $1', [id]);
+            return res.status(200).json(rows[0]);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    });
+
+router.put('/:id',
+    body('name').isLength({ min: 3 }),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+        try {
+            const id = req.params.id;
+            const { name } = req.body;
+            const result = await dbContext.query('UPDATE categories SET name=$1  WHERE id = $2 RETURNING *;'
+                , [name, id]);
+            if (result.rows.length < 1) {
+                return res.status(404).json();
+            }
+            return res.status(200).json(result.rows[0]);
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    }
+);
+router.delete('/:id',
+    async (req, res) => {
+        try {
+            const id = req.params.id;
+            const result = await dbContext.query('UPDATE categories SET status=FALSE WHERE id = $1 RETURNING *', [id])
+            return res.status(200).json(result.rows[0]);
+        } catch (error) {
+            return res.status(500).json(error)
+        }
+    }
+);
+
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ var router = express.Router();
 const auth = require('./Auth');
 const products = require('./Product');
 const homePage = require('./HomePage');
+const categories = require('./Categories');
 
 router.use('/auth', auth);
 router.use('/products', products);
-router.use('/homepage',homePage)
+router.use('/homepage',homePage);
+router.use('/categories',categories);
 
 module.exports = router;
